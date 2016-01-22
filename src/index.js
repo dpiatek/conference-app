02044be44f6931b2eb5b51ed06cf76079b951bc2
Dominik Piatek <do.piatek@gmail.com>
2016-01-22 13:01:41 +0000

    Add login capability

--- src/index.js
+++ src/index.js
@@ -2,48 +2,54 @@ import { conferences, user } from './reducers';
 import { createStore as _createStore, combineReducers } from 'redux';
 import DevTools from './dev-tools';
 import App from './components/app';
+import LoginForm from './components/login-form';
 import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import React from 'react';
+import Firebase from 'firebase';
 
+const ref = new Firebase("https://blistering-fire-6946.firebaseio.com/");
 const createStore = DevTools.instrument()(_createStore);
+const auth = ref.getAuth();
+const appRoot = document.getElementById('app');
 
-let store = createStore(combineReducers({
-  conferences,
-  user
-}), {
-  conferences: [
-    {
-      id: 0,
-      name: "BestConf",
-      topic: "Javascript",
-      website: "http://best.conf",
-      dateFrom: 1463007600000,
-      dateTo: 1463094000000,
-      peopleGoing: ["Kate"],
-      peopleInterested: ["John", "Mary"]
-    }
-  ],
-  user: {
-    name: "Dominik"
-  }
-});
-
-if (process.env.NODE_ENV === 'production') {
-  render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    document.getElementById('app')
-  )
+if (!auth) {
+  render(<LoginForm fbRef={ref} />, appRoot);
 } else {
-  render(
-    <Provider store={store}>
-      <div>
-        <App />
-        <DevTools />
-      </div>
-    </Provider>,
-    document.getElementById('app')
-  )
+  let store = createStore(combineReducers({conferences, user }), {
+    conferences: [
+      {
+        id: 0,
+        name: "BestConf",
+        topic: "Javascript",
+        website: "http://best.conf",
+        dateFrom: 1463007600000,
+        dateTo: 1463094000000,
+        peopleGoing: ["Kate"],
+        peopleInterested: ["John", "Mary"]
+      }
+    ],
+    user: {
+      name: "Dominik"
+    }
+  });
+
+  if (process.env.NODE_ENV === 'production') {
+    render(
+      <Provider store={store}>
+        <App fbRef={ref} />
+      </Provider>,
+      appRoot
+    )
+  } else {
+    render(
+      <Provider store={store}>
+        <div>
+          <App fbRef={ref} />
+          <DevTools />
+        </div>
+      </Provider>,
+      appRoot
+    )
+  }
 }
