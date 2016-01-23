3f15e4150462a83cee4f1374d3d34f62f9e980fb
Dominik Piatek <do.piatek@gmail.com>
2016-01-23 15:37:14 +0000

    Style confs

--- src/components/conf-list.js
+++ src/components/conf-list.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import Conf from './conf';
+import s from './conf-list.css';
 
 const ConfList = ({ conferences }) => {
   return (
-    <ul>
+    <ul className={s.list}>
       {conferences.map(conf => <Conf conf={conf} key={conf.id} />)}
     </ul>
   );
